6ea751073ceb396be59368a5fb540a10ec9e40bc
Joel D'Autilio <joeldautilio@gmail.com>
2023-10-06 10:03:27 -0300

    Add equals and hashCode
    Co-authored-by: PabloRoss7 <pablorossi2001@gmail.com>

--- src/main/java/structure/Schema.java
+++ src/main/java/structure/Schema.java
@@ -2,24 +2,62 @@ package structure;
 
 
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Schema {
-    public HashSet<Table> tables = null;
-    public HashSet<Procedure> procedures = null;
+    public String name;
+    public Set<Table> tables = null;
+    public Set<Procedure> procedures = null;
 
-    public void addTable(Table table) {
-        this.tables.add(table);
+    public Schema(String name) {
+        this(name, new HashSet<>(), new HashSet<>());
     }
 
-    public HashSet<Table> getTables() {
+    public Schema(String name, Set<Table> tables, Set<Procedure> procedures) {
+        this.name = name;
+        this.tables = tables;
+        this.procedures = procedures;
+    }
+
+    public void addTable(Table table) {
+        tables.add(table);
+    }
+
+    public Set<Table> getTables() {
         return tables;
     }
 
     public void addProcedure(Procedure procedure) {
-        this.procedures.add(procedure);
+        procedures.add(procedure);
     }
 
-    public HashSet<Procedure> getProcedures() {
+    public Set<Procedure> getProcedures() {
         return procedures;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Schema schema = (Schema) o;
+        return Objects.equals(name, schema.name) && Objects.equals(tables, schema.tables) && Objects.equals(procedures, schema.procedures);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, tables, procedures);
+    }
+
+    @Override
+    public String toString() {
+        return "Schema " + name + "\n" +
+                "\tTables:\n" +
+                "\t\t" + tables.stream().map(Table::toString).collect(Collectors.joining("\n\t\t")) +
+                "\n" +
+                "\nProcedures:\n" +
+                "\t\t" + procedures.stream().map(Procedure::toString).collect(Collectors.joining("\n\t\t")) +
+                "\n";
     }
 }
