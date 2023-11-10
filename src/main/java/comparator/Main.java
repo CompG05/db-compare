7224c3522eb2dd6969ce5b285c73feadf4d7a176
Joel D'Autilio <joeldautilio@gmail.com>
2023-11-09 21:30:47 -0300

    Build executable

--- src/main/java/comparator/Main.java
+++ src/main/java/comparator/Main.java
@@ -12,12 +12,28 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        if (args.length == 0 || args[0].equals("--help") || args[0].equals("-h")) {
+            System.out.println("Usage: java -jar <this jar> <config file> [-o <report file>]");
+            return;
+        }
+
+        if (args.length != 1) {
+            if (args.length != 3 || args.length == 3 && !args[1].equals("-o")) {
+                System.err.println("Usage: java -jar <this jar> <config file> [-o <report file>]");
+                return;
+            }
+        }
+        String configFilename = args[0];
+        String outputFilename = null;
+        if (args.length == 3)
+            outputFilename = args[2];
+
         DBComparator comparator;
         try {
-            JsonNode json = Utils.loadConfig("config.json");
-            Connection connection1 = Utils.getConnection("config.json", json.get("catalog1").asText());
-            Connection connection2 = Utils.getConnection("config.json", json.get("catalog2").asText());
+            JsonNode json = Utils.loadConfig(configFilename);
+            Connection connection1 = Utils.getConnection(configFilename, json.get("catalog1").asText());
+            Connection connection2 = Utils.getConnection(configFilename, json.get("catalog2").asText());
             Loader loader1 = new MyLoader(connection1, connection1.getCatalog());
             Loader loader2 = new MyLoader(connection2, connection2.getCatalog());
 
@@ -30,6 +46,9 @@ public class Main {
         }
 
         Reporter reporter = new Reporter(comparator);
-        reporter.report();
+        if (outputFilename != null)
+            reporter.report(outputFilename);
+        else
+            reporter.report();
     }
 }
